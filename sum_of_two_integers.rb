67d3e237e1920564cda6194e50eaf6f48b43aa7f
Jeff Lin <jlin28@wustl.edu>
2016-10-11 15:52:28 -0700

    sum of two integers

---
+++ sum_of_two_integers.rb
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/sum-of-two-integers/
+#
+# Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+#
+# Example:
+# Given a = 1 and b = 2, return 3.
+
+def get_sum(a, b)
+    a = Array(a).pack('l').unpack('l').first
+    b = Array(b).pack('l').unpack('l').first
+    return b == 0 ? a : get_sum(a^b, (a&b) << 1)
+end
