fbe8f7fb7adfd6d41cf6c5e78dad13c9286affb2
Jeff Lin <jlin28@wustl.edu>
2016-10-11 18:37:36 -0700

    binary tree depth

---
+++ maximum_depth_of_binary_tree.rb
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/maximum-depth-of-binary-tree/
+#
+# Given a binary tree, find its maximum depth.
+#
+# The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+def max_depth(root)
+    return 0 if root.nil?
+    [max_depth(root.left), max_depth(root.right)].max + 1
+end
