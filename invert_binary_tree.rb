95b06ac78959de3216dfb27ea165964ae932cf23
Jeff Lin <jlin28@wustl.edu>
2016-10-11 21:41:48 -0700

    invert binary tree

---
+++ invert_binary_tree.rb
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/invert-binary-tree/
+#
+# Invert a binary tree.
+#
+#      4
+#    /   \
+#   2     7
+#  / \   / \
+# 1   3 6   9
+# to
+#      4
+#    /   \
+#   7     2
+#  / \   / \
+# 9   6 3   1
+
+def invert_tree(root)
+    return nil if root.nil?
+
+    invert_tree(root.left)
+    invert_tree(root.right)
+
+    temp_node = root.left
+    root.left = root.right
+    root.right = temp_node
+    root
+end
