6c56c85b782acb4eef510d0c01980bab87a55f08
Jeff Lin <jlin28@wustl.edu>
2016-10-12 17:31:54 -0700

    array intersection

---
+++ intersection_of_two_arrays.rb
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/intersection-of-two-arrays/
+#
+# Given two arrays, write a function to compute their intersection.
+#
+# Example:
+# Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+#
+# Note:
+# Each element in the result must be unique.
+# The result can be in any order.
+
+def intersection(nums1, nums2)
+    (nums1 & nums2).uniq
+end
