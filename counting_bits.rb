9ffff5361b236236f1b20850950a0213620e5a78
Jeff Lin <jlin28@wustl.edu>
2016-10-15 19:57:14 -0700

    counting bits

---
+++ counting_bits.rb
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/counting-bits/
+#
+# Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+#
+# Example:
+# For num = 5 you should return [0,1,1,2,1,2].
+#
+# Follow up:
+#
+# It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
+# Space complexity should be O(n).
+# Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+
+def count_bits(num)
+    result = []
+    0.upto(num) do |int|
+        result << int.to_s(2).count("1")
+    end
+    result
+end
